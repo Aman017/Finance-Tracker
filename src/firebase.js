ed17f63480cd8b62988b13b4e8cff939c9edb6b9
Aman Malik <163856210+Aman017@users.noreply.github.com>
2025-08-19 19:01:13 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDsawd59SjkjSqLKCJxgwk43e51RodkusM",
   authDomain: "finance-tracker-ft-one.vercel.app",
-  projectId: "prj_Wd5eEgQ4v3fhuWoVQkd1MiGn8mL9",
+  projectId: "financly-tracker",
   storageBucket: "financly-tracker.firebasestorage.app",
   messagingSenderId: "586987463453",
   appId: "1:586987463453:web:df5266a1c6e6a0750e37f4",
