c28ddd7a5277ec9bdc3fa0e1d11dc4deed66795b
Aman Malik <163856210+Aman017@users.noreply.github.com>
2025-08-19 18:53:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,9 +8,9 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDsawd59SjkjSqLKCJxgwk43e51RodkusM",
-  authDomain: "financly-tracker.firebaseapp.com",
+  authDomain: "finance-tracker-qvce9hi5z-aman017s-projects.vercel.app",
   projectId: "financly-tracker",
-  storageBucket: "finance-tracker-qvce9hi5z-aman017s-projects.vercel.app",
+  storageBucket: "financly-tracker.firebasestorage.app",
   messagingSenderId: "586987463453",
   appId: "1:586987463453:web:df5266a1c6e6a0750e37f4",
   measurementId: "G-5G23M7BT17"
